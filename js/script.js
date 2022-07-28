906377b896df664fe632137df3108ee623a1707e
HolyMolly <104388974+hHolyMolly@users.noreply.github.com>
2022-07-29 02:43:27 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -144,6 +144,58 @@ function dynamicAdaptive() {
 }
 dynamicAdaptive(); // ДИНАМИЧЕСКИЙ АДАПТИВ
 
+const myPopup = function () {
+	const openBtns = document.querySelectorAll(".popup-open");
+	const wrappers = document.querySelectorAll(".popup-item");
+	let popupData;
+
+	if (openBtns.length > 0) {
+		openBtns.forEach(open => {
+			open.addEventListener("click", function () {
+				popupData = this.getAttribute("data-popup");
+
+				function selectPopup(popupData) {
+					wrappers.forEach(wrap => {
+						if (wrap.classList.contains(popupData)) {
+							wrap.classList.add("_active");
+							document.body.classList.add("_lock-scroll");
+						}
+					});
+				}
+				selectPopup(popupData)
+
+			});
+		});
+
+		function closePopup() {
+			const closeBtns = document.querySelectorAll(".popup-close");
+			const wrapper = document.querySelectorAll(".popup-item");
+
+			closeBtns.forEach(closeBtn => {
+				closeBtn.addEventListener("click", function () {
+					wrapper.forEach(wrap => {
+						wrap.classList.remove("_active");
+						document.body.classList.remove("_lock-scroll");
+					});
+				});
+			});
+
+			wrapper.forEach(wrap => {
+				wrap.addEventListener("click", function (e) {
+					const elementTarget = e.target;
+
+					if (!elementTarget.closest(".popup-item__body")) {
+						wrap.classList.remove("_active");
+						document.body.classList.remove("_lock-scroll");
+					}
+				});
+			});
+		}
+		closePopup()
+	}
+}
+myPopup(); // ПОПАП
+
 //< " СКРИПТЫ " >=============================================================================================================>//
 
 let isMobile = {
